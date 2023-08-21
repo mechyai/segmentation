b27bd497913b5c1f7a3c62573ba5346ce117b84f
mechyai <jpeubel.ai@gmail.com>
2023-08-20 21:15:26 -0400

    function stub for SLIC

---
+++ superpixel/SLIC.py
@@ -0,0 +1,21 @@
+"""SLIC - Simple Linear Iterative Clustering"""
+
+import numpy as np
+from typing import Union, Tuple
+
+
+def generate_SLIC_primitives(
+    image: np.ndarray,
+    k_clusters: int,
+    initialization_neighborhood: int = 3,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Peform Simple Linear Iterative Clustering (SLIC) algorithm on greyscale or RGB image
+
+    :param image: (H, W, 3) color or (H, W) greyscale image
+    :param k_clusters: Number of SLIC clusters to generate within the image
+    :param initialization_neighborhood: Square area about initially sampled cluster centers, to adjust them to the lowest gradient position
+    :return: (H, W) array of labels, (H, W) array of distances to the pixels corresponding cluster center, & (K, 2) final cluster centers
+    """
+    # Get initial sampling of cluster centers by grid
+
